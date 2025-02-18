0f4eac288ee3d2d74bf0bc33d1d01d8311fb285d
Gabrielkokolj <118972642+Gabriel-rgb55@users.noreply.github.com>
2025-02-18 11:03:30 +0100

    Create PassengerCar.java

---
+++ PassengerCar.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public abstract class PassengerCar extends Car {
+    public PassengerCar(int nrDoors, double enginePower, Color color, String modelName) {
+        super(nrDoors, enginePower, color, modelName);
+    }
+}
