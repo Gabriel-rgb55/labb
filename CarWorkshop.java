4729dc73d63e7fe41edb40b9c701842aa0c2d983
natalie-glavak <glavak@chalmers.se>
2025-02-25 11:09:45 +0100

    Update CarWorkshop.java

--- CarWorkshop.java
+++ CarWorkshop.java
@@ -1,11 +1,30 @@
 class CarWorkshop<T extends Car> {
     private Garage<T> garage;
+    private double x;
+    private double y;
 
-    public CarWorkshop(Garage<T> garage) {
-        this.garage = garage;
+
+    public CarWorkshop(int capacity) {
+        this.garage = new Garage<>(capacity);
+    }
+
+    protected void setX(double amount) {
+        x = amount;
+    }
+
+    protected void setY(double amount) {
+        y = amount;
+    }
+
+    protected double getX(){
+        return x;
+    }
+
+    protected double getY(){
+        return y;
     }
 
     public void addCar(T car) { garage.addVehicle(car); }
-    public T retrieveCar() { return garage.retrieveVehicle(); }
+    public T retrieveCar(Volvo240 car) { return garage.retrieveVehicle(); }
     public int getCarCount() { return garage.getVehicleCount(); }
 }
