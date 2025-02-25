7f97683fa853929a7fd81c76f223f22d8cf2ad64
natalie-glavak <glavak@chalmers.se>
2025-02-25 11:14:48 +0100

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -4,4 +4,11 @@ public abstract class Truck extends Car {
     public Truck(int nrDoors, double enginePower, Color color, String modelName) {
         super(nrDoors, enginePower, color, modelName);
     }
+
+    @Override
+    public void startEngine() {
+        if (Ramp.getAngle()==0) {
+            super.startEngine();
+        }
+    }
 }
