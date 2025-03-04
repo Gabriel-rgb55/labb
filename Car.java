546a0313b9a04eee390a0a27fc449717781f8d77
natalie-glavak <glavak@chalmers.se>
2025-03-04 11:57:47 +0100

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,37 +1,19 @@
 import java.awt.*;
 
-public abstract class Car implements Movable {
+public abstract class Car extends Vehicle {
     private int nrDoors;
-    private double enginePower;
-    private double currentSpeed;
     private Color color;
     private String modelName;
 
-    private double x; // X-coordinate
-    private double y; // Y-coordinate
-    private double direction; // Direction in degrees (0-359)
-
     public Car(int nrDoors, double enginePower, Color color, String modelName) {
+        super(enginePower);
         this.nrDoors = nrDoors;
-        this.enginePower = enginePower;
         this.color = color;
         this.modelName = modelName;
-        stopEngine();
-        this.x = 0;
-        this.y = 0;
-        this.direction = 0; // Facing east (0 degrees)
     }
 
     public int getNrDoors() {
         return nrDoors;
-    }
-
-    public double getEnginePower() {
-        return enginePower;
-    }
-
-    public double getCurrentSpeed() {
-        return currentSpeed;
     }
 
     public Color getColor() {
@@ -42,89 +24,9 @@ public abstract class Car implements Movable {
         this.color = color;
     }
 
-    public void startEngine() {
-        currentSpeed = 0.1;
-    }
-
-    public void stopEngine() {
-        currentSpeed = 0;
-    }
-
-    protected void incrementSpeed(double amount) {
-        currentSpeed = Math.min(currentSpeed + speedFactor() * amount, enginePower);
-    }
-
-    protected void decrementSpeed(double amount) {
-        currentSpeed = Math.max(currentSpeed - speedFactor() * amount, 0);
-    }
-
-    protected abstract double speedFactor();
-
-    public void gas(double amount) {
-        if (amount < 0 || amount > 1) {
-            throw new IllegalArgumentException("Amount must be between 0 and 1");
-        }
-        incrementSpeed(amount);
-    }
-
-    public void brake(double amount) {
-        if (amount < 0 || amount > 1) {
-            throw new IllegalArgumentException("Amount must be between 0 and 1");
-        }
-        decrementSpeed(amount);
-    }
-
-    // Movable interface implementation
     @Override
-    public void move() {
-        double radians = Math.toRadians(direction);
-        x += Math.cos(radians) * currentSpeed;
-        y += Math.sin(radians) * currentSpeed;
-    }
-
-    @Override
-    public void turnLeft() {
-        direction = (direction + 90) % 360;
-    }
-
-    @Override
-    public void turnRight() {
-        direction = (direction - 90 + 360) % 360;
-    }
-
-    protected void setX(double amount){
-        x = amount;
-    }
-
-    protected void setY(double amount){
-        y = amount;
-    }
-
-    protected void setDirection(double amount){
-        direction = amount;
-    }
-
-
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getDirection() {
-        return direction;
-    }
-
-    
-    public Point getPosition() {
-        return new Point((int) x, (int) y);
-    }
-
-    public void setPosition(Point newPosition) {
-        this.x = newPosition.x;
-        this.y = newPosition.y;
+    protected double speedFactor() {
+        // Implementera denna metod baserat på bilens specifika egenskaper
+        return getEnginePower() * 0.01; // Exempel på speedFactor
     }
 }
